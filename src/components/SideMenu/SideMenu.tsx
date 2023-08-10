94c6d227bef7d499e976da63743ae1b5029c6529
Daaski <Redkin.Artem.2017@yandex.ru>
2023-08-11 01:12:23 +0300

    'first commit'

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -15,6 +15,12 @@ export const SideMenu = () => {
     return (
         <div className="card flex justify-content-center">
             <Sidebar visible={visible} onHide={() => setVisible(false)}>
+                <Link className={scss.link_item} href={'/'}>
+                    <Button className={scss.link_button} link>
+                        <p className={scss.link_item_text}>Главная</p>
+                        <i style={{marginTop: "6px"}} className='pi pi-chevron-right'/>
+                    </Button>
+                </Link>
                 <Link className={scss.link_item} href={'/order'}>
                     <Button className={scss.link_button} link>
                         <p className={scss.link_item_text}>Оформить заявку</p>
