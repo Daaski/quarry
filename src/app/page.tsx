77cdb47ae92ae10ff14719d1c419dc30f59e164c
Daaski <Redkin.Artem.2017@yandex.ru>
2023-08-11 01:10:47 +0300

    'first commit'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main>
-        <h1 style={{textAlign: "center", marginTop: "15%"}}>Добро пожаловать на ИС "Карьер"!</h1>
+        <h1 style={{textAlign: "center", marginTop: "15%"}}>Добро пожаловать на ИС {`"Карьер!"`}</h1>
     </main>
   )
 }
